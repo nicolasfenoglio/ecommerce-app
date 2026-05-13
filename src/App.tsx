13bdaa5c747d03ca3ab8cf90bb8febd100eb2d73
Nicolás Fenoglio <57574930+fnAlfa@users.noreply.github.com>
2026-05-13 18:49:46 -0300

    build: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Hola Mundo!</h1>
+      <h1 className="text-3xl font-bold text-blue-900 text-center">
+        Hola Mundo!
+      </h1>
     </>
   );
 }
