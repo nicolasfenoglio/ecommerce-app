522dc7e7533b88312346a81f9d4ae7c364e2c168
Fran Gerber <fran198gerber@gmail.com>
2026-05-13 19:54:19 -0300

    agregamos el seleccionar categoria

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,39 @@ type Product = {
 };
 
 function App() {
+  const [categorias, SetCategorias] = useState<string[]>([]);
+  useEffect(() => {
+    fetch("https://dummyjson.com/products/category-list")
+      .then((res) => res.json())
+      .then((data) => SetCategorias(data));
+  }, []);
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
   const [products, setProducts] = useState<Product[]>([]);
   useEffect(() => {
+    if (selectedCategory) {
+      fetch(`https://dummyjson.com/products/category/${selectedCategory}`)
+        .then((res) => res.json())
+        .then((data) => setProducts(data.products));
+        return 
+    }
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
       .then((data) => setProducts(data.products));
-  }, []);
+  }, [selectedCategory]);
   return (
     <>
+     <label>
+      categorias:
+      <select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+        <option value="">Todas las categorías</option>
+        {categorias.map((categoria) => (
+          <option key={categoria} value={categoria}>
+            {categoria}
+          </option>
+        ))}
+      </select>
+
+     </label>
       {products.map((product) => (
         <div key={product.id}>
           <h2>{product.title}</h2>
